48176a939c3df17dd58c4a0bfec6598be83fecfd
raihanaafshan <raihanaafshan29@gmail.com>
2025-06-20 14:59:21 +0100

    feat: added let-var-const example

--- 01-let-var.js
+++ 01-let-var.js
@@ -2,10 +2,12 @@
 
 var age = 25;
 var age =30; // Redeclared and updated 
-
+const gender = "female";
 
 let name = "Affu";
 name = " Affuagain"; //updated 
+//gender = "male";
 
 console.log(age);
 console.log(name);
+console.log(gender);
